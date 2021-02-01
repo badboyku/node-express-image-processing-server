05d4a3720a5661ef0acc6fead204ffaab39985a4
Joey Atkinson <badboyKU@gmail.com>
2021-01-31 19:56:22 -0800

    Hook up

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
@@ -33,10 +34,14 @@ router.get('/photo-viewer', (request, response) => {
   response.sendFile(photoPath);
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) {
     response.status(400).json({ error: request.fileValidationError });
   } else {
+    try {
+      await imageProcessor(request.file.filename);
+    } catch () {
+    }
     response.status(201).json({ success: true });
   }
 });
